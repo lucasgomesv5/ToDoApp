43e8139ff1811d3e26437d6b8c3b66aafb795202
lucasgomesv5 <lucasgv5dev@gmail.com>
2023-01-23 20:12:29 -0300

    fix: global theme wasnt working on dashboard title text

--- app/src/components/text/text-style.ts
+++ app/src/components/text/text-style.ts
@@ -6,7 +6,7 @@ export const StyledText = styled.Text<TextProps>`
     font-size: ${({size}) => size};
     font-weight: ${({bold}) => bold ? 'bold' : 'normal'};
 
-    ${({white}) => white && `color: ${props => props.theme.colors.white}`}
+    ${({white}) => white && css`color: ${props=> props.theme.colors.white}`}
     ${({center}) => center && 'align-self: center'}
     ${({marginTop}) => marginTop && 'margin-top: 15px'}
     ${({mini}) => mini && css`
